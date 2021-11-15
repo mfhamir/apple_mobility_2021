8635e21bdc474304f97cf8a7519a4bb76a7a2187
Muhammed Hamir <mfhamir@usfca.edu>
2021-11-15 03:18:19 +0000

    "chnages to process seq"

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -5,21 +5,28 @@
 # November 08 2021
 
 # The following defensive programs will ensure that the user enters one and only one argument to ensure the script runs
+
 # calculate the number of data sets and entries
 
 if [ $# -eq 0 ]
 then
-  echo "To run this script, supply one arguments"
-  echo "This would be name of the file"
+  echo "To run this script, supply two arguments"
+  echo "Supply the path to the grip compressed SARS-Cov2 fasta file"
+  echo "For aditional outpu, add 'ALL' as the second argument"
   exit 1
 fi
 
-if [ $# -gt 1 ]
-then
-  echo "To run this script you are allowed to only have one argument, since that argument is the file path to the covid Sequencing Data"
+if [ $# -eq 2 ] && [ "$2 = ALL ] ;
+then print
+  echo "The total number sequencesin the file:"
+  bioawk -c fastx '{print $name $comment}' "$1" | wc -1
+  echo "This would be the total number of Sars-Cov-2 sequences per country, this will be"
+  bioawk -c fastx '{print $comment}' "$1"  |  cut -d " | " -f 21  |  sort  | uniq -c  | sort -n -r
   exit 1
 fi
 
-
-bioawk -c fastx '{print $name $comment}' "$1" | wc -l
-bioawk -c fastx '{print $comment}' "$1"| cut -d "|" -f 21 | sort | uniq -c | sort -n -r
+if [ $# -eq 1 ] ;
+then print
+  bioawk -c fastx '{print $comment}' "$1"  | cut -d " | " -f 21 | sort | uniq -c | sort -n -r
+exit 1
+fi
