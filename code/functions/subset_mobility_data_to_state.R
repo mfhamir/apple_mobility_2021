1b8641efa6e69806586acdfcaf4a1bf04028adf8
Muhammed Hamir <mfhamir@dons.usfca.edu>
2021-12-10 17:57:51 -0800

    updated dplyr in functions

--- code/functions/subset_mobility_data_to_state.R
+++ code/functions/subset_mobility_data_to_state.R
@@ -12,6 +12,7 @@
 subset_mobility_data_to_state <- function(input_file_name,
                                           state_to_subset) {
  # We will need to load the dplyr package to help us subset the data by state
+ # This will help people to run the scripts and project easier
 library("dplyr")
  # read in the compete csv file
  covid_data <- readr::read_csv(file = input_file_name)
