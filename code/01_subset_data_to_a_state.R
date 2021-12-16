e44dcc7191e719730f415d2265a99f09b2c79f9c
Muhammed Hamir <mfhamir@usfca.edu>
2021-12-16 04:37:52 +0000

    I took out the wrong line of code initally

--- code/01_subset_data_to_a_state.R
+++ code/01_subset_data_to_a_state.R
@@ -8,8 +8,10 @@
 # mfhamir@dons.usfca.edu
 # Sept 20th, 2021
 
-# load functions
-source("code/functions/subset_mobility_data_to_state.R")
+# function
+subset_mobility_data_to_state(
+  input_file_name = "data/raw_data/applemobilitytrends-2021-09-19.csv",
+  state_to_subset = "Alabama")
 
 # use function within a for loop
 for (my_state in c("Alabama", "Maryland", "Idaho", "Arizona", "Iowa")) {
